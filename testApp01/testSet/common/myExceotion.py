63807eb9d7e99c33e861717747d9e5977cc459ee
tongshan1 <1904736199@qq.com>
2015-09-30 09:53:52 +0800

    for test pychram

---
+++ testApp01/testSet/common/myExceotion.py
@@ -0,0 +1,13 @@
+# ========================================================
+# Summary        :myExceotion
+# Author         :tong shan
+# Create Date    :2015-09-28
+# Amend History  :
+# Amended by     :
+# ========================================================
+
+class myExceotion(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
